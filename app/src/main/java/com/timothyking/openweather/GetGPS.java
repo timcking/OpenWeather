ac8144f806fcdeb9482edd55db71851a59c2343a
Tim King <timcking@gmail.com>
2018-06-06 20:14:13 -0700

    New GetGPS activity

---
+++ app/src/main/java/com/timothyking/openweather/GetGPS.java
@@ -0,0 +1,13 @@
+package com.timothyking.openweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetGPS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_gps);
+    }
+}
